d1a9283fced73a6b20ef2768c7402e29b017d8e7
arjunneupane-cs <arjun.spa.np@gmail.com>
2025-02-23 00:01:30 +0530

    champ

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -14,9 +14,9 @@ const App =()=> {
   const Stack = createStackNavigator()
   return(
     <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen name="First" component={FirstPage} options={{headerShown:false}}/>
-    <Stack.Screen name="Authenticate" component={Authenticate} options={{headerShown:false}}/>
+    <Stack.Navigator initialRouteName='Main'>
+      <Stack.Screen name="First" component={FirstPage} options={{headerShown:false}}/>
+      <Stack.Screen name="Authenticate" component={Authenticate} options={{headerShown:false}}/>
       <Stack.Screen name="Main" component={Main} options={{headerShown:false}}/>
     </Stack.Navigator>
   </NavigationContainer>
