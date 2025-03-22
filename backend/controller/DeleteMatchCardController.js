f32c59f2f609ff3d5a6d631a7b5c621728ecb045
pradip kumar singh <ps6000146@gmail.com>
2025-03-22 22:33:44 +0530

    Update DeleteMatchCardController.js

--- backend/controller/DeleteMatchCardController.js
+++ backend/controller/DeleteMatchCardController.js
@@ -1,5 +1,6 @@
 const ClashSquad = require("../model/ClashSquadModel")
 const {User} = require("../model/schema")
+const tdm = require("../model/TdmModel")
 
 const deleteCard =async(req,res)=>{
 try {
@@ -12,9 +13,9 @@ if(!match){
     })
 }
 const userinfo =await User.findOne({_id:userid})
-userinfo.balance +=match.matchDetails[0].betAmount
+userinfo.balance += Number(match.matchDetails[0].betAmount)
 userinfo.isplaying =false
-userinfo.matchId =""
+userinfo.matchId.FreefireClashId.splice(0,userinfo.matchId.FreefireClashId.length)
 await userinfo.save()
 res.status(200).json({
     message:'matchcard deleted!!'
@@ -24,4 +25,27 @@ res.status(200).json({
 }
 }
 
-module.exports =deleteCard
+const deleteCardtdm =async(req,res)=>{
+    try {
+        const{matchId}=req.body
+    const userid =req.user
+    const match = await tdm.findByIdAndDelete({_id:matchId})
+    if(!match){
+        return res.status(404).json({
+            message:'matchCard not found'
+        })
+    }
+    const userinfo =await User.findOne({_id:userid})
+    userinfo.balance += Number(match.entryFee)
+    userinfo.isplaying =false
+    userinfo.matchId.pubgTdmId.splice(0,userinfo.matchId.pubgTdmId.length)
+    await userinfo.save()
+    res.status(200).json({
+        message:'matchcard deleted!!'
+    })
+    } catch (error) {
+        console.log(error)
+    }
+    }
+
+module.exports ={deleteCard,deleteCardtdm}
