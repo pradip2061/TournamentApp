0c668bf692e9b2ca56b30e496fc596f4a8004329
pradip kumar singh <ps6000146@gmail.com>
2025-03-22 22:34:21 +0530

    Update DeleteCardRouter.js

--- backend/router/user/DeleteCardRouter.js
+++ backend/router/user/DeleteCardRouter.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const {checkResult,checkuserJoinFF} = require('../../controller/checkResultController')
 const Authverify = require('../../middleware/AuthVerify')
-const deleteCard = require('../../controller/DeleteMatchCardController')
+const {deleteCard,deleteCardtdm} = require('../../controller/DeleteMatchCardController')
 const router3 = express.Router()
 router3.post('/deletecard',Authverify,deleteCard)
-
+router3.post('/deletecardtdm',Authverify,deleteCardtdm)
 module.exports = router3
