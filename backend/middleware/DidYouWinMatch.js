7454d74318dcae18c55464e234b5834a2c72571a
pradip kumar singh <ps6000146@gmail.com>
2025-03-31 11:09:57 +0530

    Update DidYouWinMatch.js

--- backend/middleware/DidYouWinMatch.js
+++ backend/middleware/DidYouWinMatch.js
@@ -6,40 +6,40 @@ const verifyDidYouWinMatch = async (req, res, next) => {
         const { matchId,boolean} = req.body;
         console.log("Received matchId:", matchId);
 
-        const match = await ClashSquad.findOne({ _id: matchId });
-        console.log("Match found:", match);
+        // const match = await ClashSquad.findOne({ _id: matchId });
+        // console.log("Match found:", match);
 
-        if (!match) {
-            return res.status(404).json({ message: "Match not found" });
-        }
+        // if (!match) {
+        //     return res.status(404).json({ message: "Match not found" });
+        // }
 
-        if(match.teamHost[0].teamHostStatus === true){
-            if(boolean === true){
-                return res.status(400).json({
-                    message:"your opponent already declared as winner report the match!!"
-                })
-            }
-        }
+        // if(match.teamHost[0].teamHostStatus === true){
+        //     if(boolean === true){
+        //         return res.status(400).json({
+        //             message:"your opponent already declared as winner report the match!!"
+        //         })
+        //     }
+        // }
 
-        if(match.teamopponent[0].team2Status === true){
-            if(boolean === true){
-                return res.status(400).json({
-                    message:"your opponent already declared as winner report the match!!"
-                })
-            }
-        }
+        // if(match.teamopponent[0].team2Status === true){
+        //     if(boolean === true){
+        //         return res.status(400).json({
+        //             message:"your opponent already declared as winner report the match!!"
+        //         })
+        //     }
+        // }
 
-        if (typeof match.teamHost[0].teamHostStatus === "boolean" &&
-            typeof match.teamopponent[0].team2Status === null) {
-                match.status = "completed";
-            return res.status(400).json({ message: "match completed!!" });
-        }
-        if (typeof match.teamHost[0].teamHostStatus === null &&
-            typeof match.teamopponent[0].team2Status === "boolean") {
-                match.status = "completed";
-            return res.status(400).json({ message: "match completed!!" });
-        }
-            await match.save();
+        // if (typeof match.teamHost[0].teamHostStatus === "boolean" &&
+        //     typeof match.teamopponent[0].team2Status === null) {
+        //         match.status = "completed";
+        //     return res.status(400).json({ message: "match completed!!" });
+        // }
+        // if (typeof match.teamHost[0].teamHostStatus === null &&
+        //     typeof match.teamopponent[0].team2Status === "boolean") {
+        //         match.status = "completed";
+        //     return res.status(400).json({ message: "match completed!!" });
+        // }
+        //     await match.save();
         next();
     } catch (error) {
         console.error("Error in verifyDidYouWinMatch:", error);
