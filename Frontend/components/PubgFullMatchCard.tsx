1745c191851eb3faae0160c5377257fdf8a543c5
pradip kumar singh <ps6000146@gmail.com>
2025-04-09 22:40:39 +0530

    Update PubgFullMatchCard.tsx

--- Frontend/components/PubgFullMatchCard.tsx
+++ Frontend/components/PubgFullMatchCard.tsx
@@ -388,23 +388,39 @@ const PubgFullMatchCard = ({matches}) => {
                         onPress={() => setReportPlayer(false)}
                         style={styles.closeButton}>
                         <Text style={styles.closeText}>X</Text>
-                        <View style={{gap:20,marginTop:20}}>
-                          {
-                            matches.gameName.map((item)=>(
-                              <View key={item._id} style={{flexDirection:'row',alignItems:'center',gap:30}}>
-                                <View>
-                                <Text>{item.player1}</Text>
-                                <Text>{item.player2 || "player2 Name Unknown"}</Text>
-                                <Text>{item.player3||"player3 Name Unknown"}</Text>
-                                <Text>{item.player4||"player4 Name Unknown"}</Text>
-                                </View>
-                                <TouchableOpacity style={{width:40,height:30,backgroundColor:"gray",alignItems:'center',justifyContent:'center'}} onPress={()=>reportplayers(item.userid)}>
-                                  <Text style={{color:'white'}}>+</Text>
-                                </TouchableOpacity>
-                                </View>
-                            ))
-                          }
-                        </View>
+                        {
+                  matches.playermode === 'Squad'?
+                  <View style={{gap:20,marginTop:20}}>
+                    {
+                      matches.gameName.map((item)=>(
+                        <View key={item._id} style={{flexDirection:'row',alignItems:'center',gap:30}}>
+                          <View>
+                          <Text>{item.player1}</Text>
+                          <Text>{item.player2 || "player2 Name Unknown"}</Text>
+                          <Text>{item.player3||"player3 Name Unknown"}</Text>
+                          <Text>{item.player4||"player4 Name Unknown"}</Text>
+                          </View>
+                          <TouchableOpacity style={{width:40,height:30,backgroundColor:"gray",alignItems:'center',justifyContent:'center'}} onPress={()=>reportplayers(item.userid)}>
+                            <Text style={{color:'white'}}>+</Text>
+                          </TouchableOpacity>
+                          </View>
+                      ))
+                    }
+                  </View>:<View style={{gap:20,marginTop:20}}>
+                    {
+                      matches.gameName.map((item)=>(
+                        <View key={item._id} style={{flexDirection:'row',alignItems:'center',gap:30}}>
+                          <View>
+                          <Text>{item.player1}</Text>
+                          </View>
+                          <TouchableOpacity style={{width:40,height:30,backgroundColor:"gray",alignItems:'center',justifyContent:'center'}} onPress={()=>reportplayers(item.userid)}>
+                            <Text style={{color:'white'}}>+</Text>
+                          </TouchableOpacity>
+                          </View>
+                      ))
+                    }
+                  </View>
+                  }
                       </TouchableOpacity>
                   </View>
                 </ScrollView>
