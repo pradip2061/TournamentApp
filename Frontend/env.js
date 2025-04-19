aebd402495b9778f281ae462d58069d187326fba
Nsagar <sagarneupane9898@gmail.com>
2025-04-19 02:45:57 -0500

    upto all

--- Frontend/env.js
+++ Frontend/env.js
@@ -2,6 +2,6 @@ const SERVER_PORT = 3000;
 const CLIENT_PORT = 3300;
 const IP = 'https://khelmela-326270540471.us-central1.run.app';
 
-const BASE_URL = 'https://khelmela-326270540471.us-central1.run.app';
+const BASE_URL = 'http://10.216.152.159:3000';
 const baseUrl = BASE_URL;
 export {SERVER_PORT, CLIENT_PORT, IP, BASE_URL, baseUrl};
